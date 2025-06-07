88c9436571e51ab988e267eef4386a813552a7fc
LAPTOP-NO9E1QHP\kws03 <kws03126@gmail.com>
2025-06-07 21:20:37 +0900

    chore: add dotenv config for environment variable support

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const dotenv = require('dotenv');
+dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
