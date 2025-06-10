8a635cf548b7978d0a45556df02ca1b3b5c180be
LAPTOP-NO9E1QHP\kws03 <kws03126@gmail.com>
2025-06-10 18:21:01 +0900

    feat: add utility to read first 3 rows from Excel sheet as 2D array

--- utils/parseExcel.js
+++ utils/parseExcel.js
@@ -1,6 +1,30 @@
 const XLSX = require('xlsx');
 const fs = require('fs');
 const path = require('path');
+const { detectFormat } = require('./formatDetector');
+
+function getFirst3Rows(filePath) { // 첫번째 시트에서 1-3행만 읽어와 2차원 배열로 리턴
+  const workbook = XLSX.readFile(filePath);
+  const sheetName = workbook.SheetNames[0];
+  const worksheet = workbook.Sheets[sheetName];
+
+  const rows = [];
+  for (let rowNum = 1 ; rowNum < 4 ; rowNum++) {
+    const row = [];
+
+    for (let col = 0 ; col <= 20 ; col++){
+      const cellAddress = XLSX.utils.encode_cell({
+        c: col,
+        r: rowNum - 1
+      });
+      const cell = worksheet[cellAddress];
+      row.push(cell ? cell.v : undefined);
+    }
+    rows.push(row);
+  }
+
+  return rows;
+}
 
 function readPersonName(filePath) {
   if (!fs.existsSync(filePath)) {
@@ -38,6 +62,7 @@ function countWorkdaysInPeriod(nameList, workLogFilePath) {
 }
 
 module.exports = {
+  getFirst3Rows,
   readPersonName,
   countWorkdaysInPeriod
 };
